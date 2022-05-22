0de4c74d25ae575fd6cca6a1ee0139679764da54
deepaksisodia9031 <106008439+deepaksisodia9031@users.noreply.github.com>
2022-05-22 13:15:12 +0530

    Add files via upload

---
+++ My First PYTHON PROGRAM.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('Hello World')
+
+
+# In[3]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+print("Father's name")
+
+
+# In[6]:
+
+
+x = 10
+print('x')
+
+
+# In[7]:
+
+
+x = 10
+print(x)
+
+
+# In[8]:
+
+
+print('Value of x = ', x)
+
+
+# In[11]:
+
+
+y = 20
+print('x + Y', x + y)
+
+
+# In[ ]:
+
+
+
+
